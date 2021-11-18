4085e8b1eddd23eaee9d06dd9dc245236db708ec
USER <bayzid96@gmail.com>
2021-11-18 14:01:10 +0600

    admin

---
+++ app/src/main/java/com/example/adminpannel/NoticeActivity.java
@@ -0,0 +1,24 @@
+package com.example.adminpannel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+
+        getSupportActionBar().setTitle("Go Back");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
